72d120ac271730029736f8b03687afb09d44f0e3
Kjuh28 <kjuh28@gmail.com>
2025-01-20 10:18:19 -0300

    feat: (abilitie) create abilitie_model file and configure it

---
+++ src/models/abilitie_model.ts
@@ -0,0 +1,44 @@
+import { sequelizeDbConfig } from "../database/database_connection";
+import { Model, DataTypes } from "sequelize";
+
+class Abilitie extends Model{
+    declare id: string
+    declare id_adm: string
+    declare title: string
+    declare icon_link: string 
+    declare description: string
+}
+
+Abilitie.init({
+    id:{
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true
+    },
+    id_adm:{
+        type: DataTypes.UUID,
+        allowNull: false,
+        references:{
+            model: 'admin',
+            key:'id'
+        }
+    },
+    title:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    icon_link:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+},{
+    sequelize: sequelizeDbConfig,
+    tableName: 'abilities',
+    timestamps: false
+})
+
+export default Abilitie
