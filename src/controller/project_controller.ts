9105775aa7d67c0e603d7dc6866da3e27e350d26
Kjuh28 <kjuh28@gmail.com>
2025-01-21 13:38:47 -0300

    feat: (editProject) add data.save function to change values in database

--- src/controller/project_controller.ts
+++ src/controller/project_controller.ts
@@ -2,17 +2,7 @@ import { randomUUID } from "node:crypto";
 import Project from "../models/project_model.js";
 import { FastifyRequest, FastifyReply } from "fastify";
 
-interface ProjectType{
-    id: string,
-    id_adm: string,
-    title: string,
-    description: string,
-    git_link: string,
-    page_link: string,
-    image: string
-}
-
-export async function createProject(req: FastifyRequest<{Body: ProjectType}> , resp: FastifyReply){
+export async function createProject(req: FastifyRequest<{Body: Project}> , resp: FastifyReply){
     await Project.create({
         id: randomUUID(),
         id_adm: req.body.id_adm,
@@ -24,7 +14,7 @@ export async function createProject(req: FastifyRequest<{Body: ProjectType}> , r
     })
 }
 
-export async function getProjectByTitle(req: FastifyRequest<{Params: ProjectType}>, resp: FastifyReply){
+export async function getProjectByTitle(req: FastifyRequest<{Params: Project}>, resp: FastifyReply){
     const findByTitle = req.params.title
     const data = await Project.findOne({where: { title: findByTitle}});
     
@@ -55,7 +45,7 @@ export async function getAllProjects(req: FastifyRequest, resp: FastifyReply){
     }
 }
     
-export async function editProject(req: FastifyRequest<{Body: ProjectType, Params: ProjectType}>, resp:FastifyReply){
+export async function editProject(req: FastifyRequest<{Body: Project, Params: Project}>, resp:FastifyReply){
     const projectId = req.params.id
     const id_adm = req.params.id_adm
 
@@ -65,14 +55,13 @@ export async function editProject(req: FastifyRequest<{Body: ProjectType, Params
         if(!data){
             resp.send('Projeto não encontrado!')
         }
-        // console.log('----------------------')
-        // console.log(data?.title)
-        // req.body.title ? (data?.title = req.body.title) : data?.dataValues.title
-        // req.body.description ? data?.description : req.body.description
-        // req.body.git_link ? data?.git_link : req.body.git_link
-        // req.body.image ? data?.image : req.body.image
+        req.body.title ? (data!.title = req.body.title) : data?.dataValues.title
+        req.body.description ? (data!.description = req.body.description) : req.body.description
+        req.body.git_link ? (data!.git_link = req.body.git_link ) : req.body.git_link
+        req.body.image ? (data!.image = req.body.image) : req.body.image
+        req.body.page_link ? (data!.page_link = req.body.page_link) : req.body.page_link
 
-        // await data?.save({fields: ['title', 'description', 'git_link', 'image']})
+        await data?.save({fields: ['title', 'description', 'git_link', 'page_link' , 'image']})
 
     } catch (error) {
         resp.status(404).send('Pagina não encontrada!')
