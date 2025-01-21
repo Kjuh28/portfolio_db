ddc13130e9f364804f8d7dc226ac8ea620bc9eb2
Kjuh28 <kjuh28@gmail.com>
2025-01-21 13:42:11 -0300

    feat: (AbilitieRouter) add AbilitieRouter function in router's path

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,6 +1,8 @@
 import {FastifyInstance} from "fastify";
 import projectRouter from "./project_route.js";
+import AbilitieRouter from "./abilitie_route.js";
 
 export default async function routers(app: FastifyInstance){
     app.register(projectRouter, {prefix: '/api'})
+    app.register(AbilitieRouter, {prefix: '/api'})
 }
