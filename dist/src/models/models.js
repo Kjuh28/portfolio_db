afccc5f633430a3947f1e3758d1332cb6cf24c79
Kjuh28 <kjuh28@gmail.com>
2025-01-24 17:43:18 -0300

    refactor (dist) change name from dirt to dist in deployment

---
+++ dist/src/models/models.js
@@ -0,0 +1,9 @@
+import { Sequelize } from "sequelize";
+import Project from "./project_model.js";
+import Abilitie from "./abilitie_model.js";
+const dbModel = {
+    Project,
+    Abilitie,
+    Sequelize
+};
+export default dbModel;
