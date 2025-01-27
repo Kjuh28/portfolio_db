5b6915ab8ebf2e2c6f36f3cb9885b5e896302c42
Kjuh28 <kjuh28@gmail.com>
2025-01-27 14:16:26 -0300

    refactor: implementation attempt

--- app.ts
+++ app.ts
@@ -7,6 +7,11 @@ import cors from "@fastify/cors";
 
 const server = fastify()
 
+export default async function handler(req: any, res: any) {
+    await server.ready()
+    server.server.emit('request', req, res)
+}
+
 server.register(cors)
 
 const portEnv = process.env.PORT ? parseInt(process.env.PORT, 10) : undefined
