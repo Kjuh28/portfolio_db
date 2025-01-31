d6fd0e82bcdc2590c65c8e9822e17408c13e7821
Kjuh28 <kjuh28@gmail.com>
2025-01-31 15:22:49 -0300

    refactor: (vercel-deploy) implementation attempt

--- src/database/database_connection.ts
+++ src/database/database_connection.ts
@@ -1,11 +1,12 @@
 import { Sequelize } from 'sequelize'
 import 'dotenv/config'
-
+const pg = require('pg')
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env
 
 export const sequelizeDbConfig = new Sequelize(
     PGDATABASE!, PGUSER!, PGPASSWORD, {
         dialect: 'postgres',
+        dialectModule: pg,
         host: PGHOST,
         dialectOptions: {
             ssl: true
